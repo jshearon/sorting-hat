2c74f3f41f4c5fea025463d61afe60e1b146cb49
Jonathan Shearon <jonathan.shearon@gmail.com>
2020-05-01 01:22:57 -0500

    created form function

---
+++ arrays.js
@@ -0,0 +1,61 @@
+const houses = [
+  { 
+    name: 'Gryffindor', 
+    color: 'red',
+    crest: 'gryffindor-crest.png'
+  },
+  {
+    name: 'Hufflepuff', 
+    color: 'yellow',
+    crest: 'hufflepuff-crest.png'
+  },
+  {
+    name: 'Ravenclaw', 
+    color: 'blue',
+    crest: 'ravenclaw-crest.png'
+  },
+  {
+    name: 'Slytherin', 
+    color: 'green',
+    crest: 'slytherin-crest.png'
+  }
+]
+  
+const students = [
+  {
+    name: 'Luna Lovegood',
+    house: 'Ravenclaw',
+    expelled: false,
+    unique: 1588312600089
+  },
+  {
+    name: 'Neville Longbottom',
+    house: 'Gryffindor',
+    expelled: false,
+    unique: 1588312603950
+  },
+  {
+    name: 'Ginny Weasley',
+    house: 'Gryffindor',
+    expelled: false,
+    unique: 1588312605722
+  },
+  {
+    name: 'Hermione Granger',
+    house: 'Gryffindor',
+    expelled: false,
+    unique: 1588312606542
+  },
+  {
+    name: 'Draco Malfoy',
+    house: 'Slytherin',
+    expelled: true,
+    unique: 1588313343516
+  },
+  {
+    name: 'Teddy Lupin',
+    house: 'Hufflepuff',
+    expelled: true,
+    unique: 1588313344420
+  }
+]
--- main.js
+++ main.js
@@ -1,16 +1,63 @@
-const imageShake = (id) => {
-  const element = document.getElementById(id);
+//prints strings to the specified div id
+const printToDom = (divId, textString) => {
+  document.getElementById(divId).innerHTML = textString;
+}
+
+
+//activates then removes the css image shake class
+const imageShake = () => {
+  const element = document.getElementById('sorting-hat');
     element.classList.remove("image-shake");
     void element.offsetWidth;
     element.classList.add("image-shake");
 }
 
-const sortStudents = () => {
-  imageShake("sorting-hat");
+//get a random house name
+const randomHouse = () => {
+  return houses[Math.floor(Math.random() * (3 - 0 + 1)) + 0].name;
 }
 
+//creates and prints the sorting form
+const createForm = () => { 
+  domString = `<form class="form-inline cards border" id="submitNameForm">
+                <div class="form-group">
+                  <label class="sr-only" for="studentName">Name</label>
+                  <input type="text" class="form-control mb-2 mr-sm-2" id="studentName" placeholder="Harry Potter">
+                  <button type="submit" class="btn btn-primary mb-2" id="submitName">Find Your House!</button>
+                </div>
+                </form>`;
+  printToDom('sorting-form', domString);
+  document.getElementById('submitName').addEventListener("click", sortStudents);
+}
+
+//creates and prints student cards from array
+const createStudentCards = () => { 
+  domString = `<form class="form-inline cards border" id="submitNameForm">
+                <div class="form-group">
+                  <label class="sr-only" for="studentName">Name</label>
+                  <input type="text" class="form-control mb-2 mr-sm-2" id="studentName" placeholder="Harry Potter">
+                  <button type="submit" class="btn btn-primary mb-2" id="submitName">Find Your House!</button>
+                </div>
+                </form>`;
+  printToDom('sorting-form', domString);
+}
+
+//creates the sorting form and existing students in array
+const sortStudents = (event) => {
+  const studentName = event.target.form.elements.studentName.value;
+  if (studentName == '') {
+    $('#no-name-dialog').modal('show');
+    return
+  } else {
+    const newStudent = {name: studentName, house: randomHouse(), expelled: false, unique: Date.now()};
+    students.push(newStudent);
+  }
+  return false;
+}
+
+// event listener to show form
 const sortClick = (id) => {
-  document.getElementById(id).addEventListener("click", sortStudents)
+  document.getElementById(id).addEventListener("click", createForm);
 }
 
 const init = () => {
